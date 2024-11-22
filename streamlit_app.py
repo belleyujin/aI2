64afa0249a80c8a7474afce392ddc73595f2895a
belleyujin <20242809@sdhs.gwe.hs.kr>
2024-11-22 09:26:24 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from PIL import Image
 import gdown
 
 # Google Drive 파일 ID
-file_id = '1to0i7QzyRhDlriWbAxJpsmqXtRlcQsVN'
+file_id = '1KRkk0WU88JO6Tv5GHvBspDDDSo2yM5-Y'
 
 # Google Drive에서 파일 다운로드 함수
 @st.cache(allow_output_mutation=True)
